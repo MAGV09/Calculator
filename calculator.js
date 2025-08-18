07306b745c1fdcd5a8ec20f08ae8dc6fbd0c8496
MAGV09 <mohamedadel2002320@gmail.com>
2025-08-18 17:15:59 +0300

    add logic for the rest of the numbers

--- calculator.js
+++ calculator.js
@@ -1,5 +1,71 @@
 'use strict';
 
+const hero = document.querySelector('#main-hero');
+const clear = document.querySelector('#clear-btn');
+const displayVal = document.querySelector('#display-value');
+let check = false;
+let check2 = false;
+let dotSeperator = true;
+hero.addEventListener('click', (e) => {
+  function evaluate() {
+    const operation = displayVal.textContent;
+    const op = operation.includes('+')
+      ? '+'
+      : operation.includes('-')
+      ? '-'
+      : operation.includes('**')
+      ? '**'
+      : operation.includes('/')
+      ? '/'
+      : operation.includes('*')
+      ? '*'
+      : 'ERR';
+    if (op !== 'ERR' && !isNaN(operation.at(-1))) {
+      const i = operation.indexOf(op);
+      const num1 = +operation.slice(0, i);
+      const operator = op;
+      const num2 = +operation.slice(i + op.length);
+      displayVal.textContent = operate(num1, operator, num2);
+    }
+  }
+  switch (e.target.id) {
+    case 'clear-btn':
+      displayVal.textContent = '';
+      break;
+    case 'dot-btn':
+      if (displayVal.textContent.at(-1) !== '.' && dotSeperator) {
+        displayVal.textContent += '.';
+        dotSeperator = false;
+      }
+      break;
+    case 'equal-btn':
+      evaluate();
+      check = false;
+      check2 = true;
+      break;
+    default:
+      if (
+        e.target.textContent === '+' ||
+        e.target.textContent === '-' ||
+        e.target.textContent === '/' ||
+        e.target.textContent === '*' ||
+        e.target.textContent === '**'
+      ) {
+        if (check) evaluate();
+        displayVal.textContent += e.target.textContent;
+        check = true;
+        dotSeperator = true;
+        check2 =false
+      } else {
+        if (check2) {
+          displayVal.textContent = '';
+        }
+        displayVal.textContent += e.target.textContent;
+        check2 =false
+      }
+  }
+});
+
 const add = (num1, num2) => num1 + num2;
 
 const subtract = (num1, num2) => num1 - num2;
@@ -8,22 +74,27 @@ const multiply = (num1, num2) => num1 * num2;
 
 const divide = (num1, num2) => num1 / num2;
 
+const power = (num1, num2) => num1 ** num2;
+
 function operate(num1, operator, num2) {
-    let result
+  let result;
   switch (operator) {
     case '+':
-     result= add(num1, num2);
+      result = add(num1, num2);
       break;
     case '-':
-     result= subtract(num1, num2);
+      result = subtract(num1, num2);
       break;
     case '*':
-     result= multiply(num1, num2);
+      result = multiply(num1, num2);
       break;
     case '/':
-      result=divide(num1, num2);
+      result = divide(num1, num2);
       break;
+    case '**':
+      result = power(num1, num2);
   }
-  return result
+  result = result % 1 === 0 ? result : floor2Decimal(result);
+  return result;
 }
-console.log(operate(1,'+',3));
+const floor2Decimal = (num) => Math.floor(num * 100) / 100;
